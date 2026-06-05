0996c6d2841bddeea22d6a2b856c819d9cb6a8a2
ndahiroloicke <ndahiroloicke@gmail.com>
2026-06-05 09:58:45 +0200

    first of many

---
+++ src/test/java/com/example/uums/UumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
